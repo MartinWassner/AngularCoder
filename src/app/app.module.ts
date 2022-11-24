da51a705f502ab6aae88a693712151ba08bfd9ec
Martin Wassner <wassner.martin@gmail.com>
2022-11-24 17:13:58 -0300

    Definición de secciones, incorporado BS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TopComponent } from './clase1/top/top.component';
+import { LeftMenuComponent } from './clase1/left-menu/left-menu.component';
+import { MainComponent } from './clase1/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopComponent,
+    LeftMenuComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
